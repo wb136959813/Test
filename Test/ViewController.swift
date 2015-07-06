e6745218d40672db87e2a81fb7079c68ac0f5d03
kingbin <shann136959813@163.com>
2015-07-06 01:52:41 -0700

    优化信息显示的坐标

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var ScreenX: CGFloat = 0.0
-    var ScreenY: CGFloat = 0.0
+    var ScreenX: CGFloat = 0.0//screen coordinate x
+    var ScreenY: CGFloat = 0.0//screen coordinate y
+    var MsgNum = 0//user messages total
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
         ScreenY = UIScreen.mainScreen().bounds.height
         
         UserMsg()
+        UserMsg()
+        UserMsg()
+        UserMsg()
+        UserMsg()
+        //SetNav()
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,30 +45,32 @@ class ViewController: UIViewController {
         
         SetMsgWidgetVote()
         SetMsgWidgetVoteBtn()
+        
+        MsgNum++
     }
     
     //set messsage widget background
     func SetMsgWidgetBackground(){
-        var ImgBg = UIImageView(frame: CGRectMake(ScreenX*0.1, 40, ScreenX*0.8, 400))
+        var ImgBg = UIImageView(frame: CGRectMake(ScreenX*0.1, CGFloat(40 + 405*MsgNum), ScreenX*0.8, 400))
             ImgBg.layer.borderWidth = 1
             ImgBg.layer.borderColor = UIColor.grayColor().CGColor
             self.view.addSubview(ImgBg)
     }
     //set messsage widget user head Image
     func SetMsgWidgetHead(){
-        var ImgHead = UIImageView(frame: CGRectMake((ScreenX*0.1)+2, 42, 45, 45))
+        var ImgHead = UIImageView(frame: CGRectMake((ScreenX*0.1)+2, CGFloat(42 + 405*MsgNum), 45, 45))
             ImgHead.image = UIImage(named: "male.jpg")
             self.view.addSubview(ImgHead)
     }
     //set messsage widget user Nick
     func SetMsgWidgetNick(){
-        var LabNick = UILabel(frame: CGRectMake(90, 52, 100, 20))
+        var LabNick = UILabel(frame: CGRectMake(90, CGFloat(52 + 405*MsgNum), 100, 20))
             LabNick.text = "tester"
             self.view.addSubview(LabNick)
     }
     //set message widget Image
     func SetMsgWidgetImgCont(){
-        var ImgCont = UIImageView(frame: CGRectMake((ScreenX*0.1)+2, 95, ScreenX*0.8 - 4, 200))
+        var ImgCont = UIImageView(frame: CGRectMake((ScreenX*0.1)+2, CGFloat(95 + 405*MsgNum), ScreenX*0.8 - 4, 200))
             ImgCont.layer.borderWidth = 1
             ImgCont.layer.borderColor = UIColor.grayColor().CGColor
             ImgCont.image = UIImage(named: "female.jpg")
@@ -70,7 +78,7 @@ class ViewController: UIViewController {
     }
     //set message widget Image descprition
     func SetMsgWidgetImgDesc(){
-        var TextCont = UITextView(frame: CGRectMake((ScreenX*0.1)+2, 300, ScreenX*0.8 - 4, 50))
+        var TextCont = UITextView(frame: CGRectMake((ScreenX*0.1)+2, CGFloat(300 + 405*MsgNum), ScreenX*0.8 - 4, 50))
             TextCont.layer.borderWidth = 1
             TextCont.layer.borderColor = UIColor.grayColor().CGColor
             TextCont.text = "testtesttestte\r\nsttesttesttesttesttes\r\nttesttesttesttest"
@@ -78,27 +86,27 @@ class ViewController: UIViewController {
     }
     //set message widget vote show
     func SetMsgWidgetVote(){
-        var LabVote = UILabel(frame: CGRectMake((ScreenX*0.1)+2, 365, ScreenX*0.8, 10))
+        var LabVote = UILabel(frame: CGRectMake((ScreenX*0.1)+2, CGFloat(365 + 405*MsgNum), ScreenX*0.8, 10))
             LabVote.text = "有用 100 － 没用 100  评论 100"
             self.view.addSubview(LabVote)
     }
     //set message widget Vote button
     func SetMsgWidgetVoteBtn(){
-        var BtnGood = UIButton(frame: CGRectMake((ScreenX*0.1)+5, 380, 40, 40))
+        var BtnGood = UIButton(frame: CGRectMake((ScreenX*0.1)+5, CGFloat(380 + 405*MsgNum), 40, 40))
         BtnGood.setImage(UIImage(named: "good.png"), forState: UIControlState.Normal)
         BtnGood.setTitle("BtnGood", forState: UIControlState.Normal)
         self.view.addSubview(BtnGood)
         
         BtnGood.addTarget(self, action: Selector("OnGoodBtn:"), forControlEvents: UIControlEvents.TouchUpInside)
         
-        var BtnBad = UIButton(frame: CGRectMake((ScreenX*0.1)+85, 380, 40, 40))
+        var BtnBad = UIButton(frame: CGRectMake((ScreenX*0.1)+85, CGFloat(380 + 405*MsgNum), 40, 40))
         BtnBad.setImage(UIImage(named: "bad.png"), forState: UIControlState.Normal)
         BtnBad.setTitle("BtnBad", forState: UIControlState.Normal)
         self.view.addSubview(BtnBad)
         
         BtnBad.addTarget(self, action: Selector("OnBadBtn:"), forControlEvents: UIControlEvents.TouchUpInside)
         
-        var BtnComment = UIButton(frame: CGRectMake((ScreenX*0.1)+165, 380, 40, 40))
+        var BtnComment = UIButton(frame: CGRectMake((ScreenX*0.1)+165, CGFloat(380 + 405*MsgNum), 40, 40))
         BtnComment.setImage(UIImage(named: "msg"), forState: UIControlState.Normal)
         BtnComment.setTitle("BtnComment", forState: UIControlState.Normal)
         self.view.addSubview(BtnComment)
@@ -112,6 +120,13 @@ class ViewController: UIViewController {
             TextComment.layer.borderColor = UIColor.grayColor().CGColor
         self.view.addSubview(TextComment)
     }
+    //set Navigation widget
+    func SetNav(){
+        var ImgNav = UIImageView(frame: CGRectMake(0, ScreenY - 45, ScreenX, 45))
+            ImgNav.layer.backgroundColor = UIColor.redColor().CGColor
+        self.view.addSubview(ImgNav)
+    }
+    
     
     //click OnGoodBtn button
     func OnGoodBtn(Btn:UIButton){
